f7c46d2c4582de00b6e47d9999d771eb894e6a46
renan <ren3alves@gmail.com>
2025-11-09 19:20:09 -0300

    Modelo do professor feito

--- src/modelo/Professor.java
+++ src/modelo/Professor.java
@@ -1,5 +1,12 @@
 package modelo;
 
 public class Professor {
-
+	private String nomeProfessor;
+	private String CPFprofessor;
+	private String RGprofessor;
+	private String dataNascimentoProfessor;
+	private String ruaProfessor;
+	private String numCasaProfessor;
+	private String bairroProfessor;
+	private String cidadeProfessor;
 }
